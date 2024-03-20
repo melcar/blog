eac1646d7006b37eed82e2c60a9a314d0972c90a
melcar <15875473+melcar@users.noreply.github.com>
2024-03-20 18:39:56 +0100

    using blog subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://beltus.be',
+	site: 'https://blog.beltus.be',
 	integrations: [mdx(), sitemap()],
 });
